df93453c91735864be890ea26073645b2a9104f1
jeffreyadamo <jeffreyadamo@gmail.com>
2020-07-11 14:39:05 -0700

    heroku fix1

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
-
-mongoose.connect(MONGODB_URI, {
+var MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
